04d3bba26b83809cf7b8e86956113d0fc60e71aa
Srini M <62348832+srinimkrd@users.noreply.github.com>
2026-04-09 00:00:04 +0530

    Add files via upload

---
+++ python_basics_Srini.py
@@ -0,0 +1,22 @@
+# Task 1: Variables and Data Types
+name = "Srini"
+age = 25
+height = 1.75
+is_student = True
+print(name, age, height, is_student)
+
+# Task 2: String Formatting(F string)
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+# Task 3: Arithmetic Operations
+# Age calculations
+age_in_months = age * 12
+age_in_days = age * 365
+age_mod_7 = age % 7
+age_squared = age ** 2
+
+print("\n--- Age Calculations ---")
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Age % 7:", age_mod_7)
+print("Age squared:", age_squared)
