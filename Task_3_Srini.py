ad06ae4db2cb0b28dfca2107dca191d3c6d14ed5
Srini M <62348832+srinimkrd@users.noreply.github.com>
2026-04-09 21:55:00 +0530

    Add files via upload

---
+++ Task_3_Srini.py
@@ -0,0 +1,16 @@
+# Task 1
+print("Task 1 start.")
+for i in range(1, 6):
+    print(i)
+
+# Task 2
+print("\nTask 2 start.")
+for j in range(1, 11):
+    if j % 2 != 0:
+        print(f"{j} is odd")
+
+#task 3
+print("\nTask 3 start.")
+for k in range(3):
+    for l in range(2):
+        print(f"{k}, {l}")
